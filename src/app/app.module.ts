6c3e6de3c6fa8b63052ac1ac026433c3920fd58e
feature <mwmw654@qq.com>
2017-10-31 22:15:10 +0800

    refactor sth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {ConcordanceComponent} from './post/concordance.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ConcordanceComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ConcordanceComponent]
 })
 export class AppModule {
 }
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {ConcordanceComponent} from './concordance.component';
+import {PostComponent} from './post.component';
 
 const PostRoutes: Routes = [
-    { path: '', component: TimelineComponent },
-    { path: ':type', component: ConcordanceComponent },
-    { path: ':type/:id', component: PostComponent }
+  {path: '', component: ConcordanceComponent},
+  {path: ':id', component: PostComponent}
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forChild(PostRoutes)
-    ],
-    exports: [RouterModule]
+  imports: [
+    RouterModule.forChild(PostRoutes)
+  ],
+  exports: [RouterModule]
 })
-export class PostRoutingModule { }
+export class PostRoutingModule {
+}
